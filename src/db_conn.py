5602b425fab81f51f1dd37538da12378431dfa73
kdgursahani <144947129+kdgursahani@users.noreply.github.com>
2024-10-17 20:39:45 -0500

    Delete src directory

--- src/db_conn.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def get_db_conn():
-    """
-    Returns a connection to the SQLite database.
-    """
-    db_path = 'data.db'
-    conn = sqlite3.connect(db_path)
-    return conn
